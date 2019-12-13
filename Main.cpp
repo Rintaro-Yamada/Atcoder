2a1347221669eddd3bc5c118ca6651c493d4fe80
Rintaro-Yamada <49467661+Rintaro-Yamada@users.noreply.github.com>
2019-12-13 12:12:12 +0900

    雛形

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-using pii  = pair<int,int>;
-#define rep(i,n) for(ll i=0; i<(n); i++)
-#define rep2(i,x,n) for(ll i=x; i<(n); i++)
-#define MOD 1000000007
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-const long long INF = 1LL << 60;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep2(i, a, b) for (int i = (a); i < (b); ++i)
+#define MAX 128
+#define fst first
+#define sec second
+#define pb push_back
+#define mp make_pair
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+#define bit(n) (1LL<<(n))
+#define int long long
 const double PI = 3.141593;
-ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}    //最大公約数
-ll lcm(ll a,ll b) {return a * b / gcd(a, b);}   //最小公倍数
-ll combination(ll n, ll r) {
-  if ( r * 2 > n ) r = n - r;
+const int MOD = (int)1e9 + 7;
+const int INF = 100100100;
+const double EPS = 1e-9;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; } //最大公約数
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }     //最小公倍数
+ll combination(ll n, ll r)
+{
+  if (r * 2 > n)
+    r = n - r;
   ll dividend = 1;
-  ll divisor  = 1;
-  for ( ll i = 1; i <= r; ++i ) {
-    dividend *= (n-i+1);
-    divisor  *= i;
+  ll divisor = 1;
+  for (ll i = 1; i <= r; ++i)
+  {
+    dividend *= (n - i + 1);
+    divisor *= i;
   }
   return dividend / divisor;
 }
 
-int main(){
+signed main()
+{
 
+  return 0;
 }
